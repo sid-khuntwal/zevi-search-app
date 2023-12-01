71a9e78cbeee6efe918cc1f6a9a60b05f68bc8c2
Siddharth Khuntwal <achillesid34@gmail.com>
2023-12-01 19:02:46 +0530

    Faker data + Pages + Routes Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
